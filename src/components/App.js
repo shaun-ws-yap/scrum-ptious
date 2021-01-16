55d854e622b54c337873aa81fac103f24d62c914
Kevinli296 <kevinli296@outlook.com>
2021-01-16 21:53:57 +0000

    Create basic layout for sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
 import '../styles/App.css';
-import { Route, BrowserRouter as Router, withRouter } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch, withRouter } from 'react-router-dom';
 
 import Sidebar from './Sidebar';
+import Dashboard from './Dashboard';
 
 import "../styles/Sidebar.css";
 
 function App() {
   return (
-    <main className="layout">
-      <section className="sidebar">
-        <Sidebar />
-      </section>
-    </main>
+    <>
+    {/* <main className="layout">
+      <section className="sidebar"> */}
+        <Router>
+          <Sidebar />
+          <Switch>
+            <Route path='/' exact component={Dashboard}/>
+            {/* <Route path='/' component={Tasks}/>
+            <Route path='/' component={Teamchat}/> */}
+          </Switch>
+        </Router>
+      {/* </section>
+    </main> */}
+    </>
   );
 }
 
-export default App;
+export default App
