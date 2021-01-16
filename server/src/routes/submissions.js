3d449b217404d35200a9464e9d5ad7bb039592bd
Kevinli296 <kevinli296@outlook.com>
2021-01-16 10:00:28 +0000

    Add post route

--- server/src/routes/submissions.js
+++ server/src/routes/submissions.js
@@ -1,21 +1,34 @@
 const router = require("express").Router();
 
 module.exports = (db) => {
+  // Get all submissions
   router.get("/submissions", (req, res) => {
     const queryString = `
     SELECT
-      feedback_string,
-      submission_date,
-      task_id
+    *
     FROM submissions
-    JOIN tasks ON task_id = tasks.id
     ORDER BY submission_date
     `;
     db.query(queryString)
     .then(data => {
       res.json(data.rows);
-    });
+    })
+    .catch(err => res.send(err));
   });
+
+  // Create a submission
+  router.post("/submissions/:id", (req, res) => {
+    const { feedback_string, submission_date, task_id } = req.body;
+    const queryString = `
+    INSERT INTO submissions (feedback_string, submission_date, task_id)
+    VALUES ($1, $2, $3)
+    RETURNING *
+    `;
+    db.query(queryString, [feedback_string, submission_date, task_id])
+    .then(data => {
+      res.json(data.rows[0])
+    })
+  })
 
   return router;
 }
