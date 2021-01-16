90604731475244170733629187eb30dd29d68b16
Shaun Yap <me@shaunyap.ca>
2021-01-15 22:39:03 -0800

    Add more task routes

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -27,8 +27,33 @@ module.exports = (db) => {
     .catch(e => res.send(e));
   })
 
-  // Update task by id
-  
+  // Create and edit task
+  router.put("/tasks/:id", (req, res) => {
+    const { params } = req.body;
+
+    const queryString = `
+    INSERT INTO tasks (title, description, due_date, employee_id)
+    VALUES ($1, $2, $3, $4)
+    ON CONFLICT (id) DO
+    UPDATE SET title = $1, description = $2, due_date = $3, employee_id = $4
+    `;
+
+    db.query(queryString, [params.title, params.description, params.due_date, params.employee_id])
+    .then(data => {
+      res.status(204).json({});
+    })
+    .catch(e => res.send(e));
+  })
+
+  // Delete task by id
+  router.delete("/tasks/:id", (req, res) => {
+    db.query("DELETE FROM tasks WHERE id = $1", [req.params.id])
+    .then(data => {
+      res.status(204);
+    })
+    .catch(e => res.send(e));
+  })
+
 
   return router;
 }
