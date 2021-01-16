cbc687abcfc8fad6acc34a9f870c9cf2b2768ed3
Kevinli296 <kevinli296@outlook.com>
2021-01-16 10:05:06 +0000

    Add comments/clean up get route

--- server/src/routes/employees.js
+++ server/src/routes/employees.js
@@ -1,17 +1,12 @@
 const router = require("express").Router();
 
 module.exports = (db) => {
+  // Get all employees
   router.get("/employees", (req, res) => {
     const queryString = `
     SELECT
-      name,
-      email,
-      phone_number,
-      role,
-      team_id,
-      team_name
+    *
     FROM employees
-    JOIN teams ON team_id = teams.id
     ORDER BY role
     `;
     db.query(queryString)
