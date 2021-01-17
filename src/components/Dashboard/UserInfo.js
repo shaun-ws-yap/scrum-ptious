472d40e673b9e993cc4ffab524c88c320ecb74e5
Kevinli296 <kevinli296@outlook.com>
2021-01-17 11:51:18 +0000

    Add deadlinelist/item component for displaying upcoming deadlines in user info panel

--- src/components/Dashboard/UserInfo.js
+++ src/components/Dashboard/UserInfo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import DeadlineList from './DeadlineList';
+
 export default function UserInfo(props) {
   return (
     <div className="user-info">
@@ -8,8 +10,11 @@ export default function UserInfo(props) {
       <img src="https://randomuser.me/api/portraits/men/73.jpg" className="user-avatar"></img>
       <h4>Welcome, #Name</h4>
       <div>
-        Task in progress
-        Deadline
+        Task Deadlines:
+        <br />
+        <div>
+          <DeadlineList />
+        </div>
       </div>
     </div>
   )
