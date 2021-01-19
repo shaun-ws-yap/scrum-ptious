6ba59a6a8117714f6689235ab6a42b4d47dbb481
clarchiu <clarchiu@gmail.com>
2021-01-18 22:43:56 -0800

    set up socket connection between client and server

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 
 import ChatLog from "./ChatLog";
 import MembersList from "./MembersList";
@@ -6,14 +6,30 @@ import InputBox from "./InputBox";
 
 import '../../styles/Chat.css';
 
+import { io } from 'socket.io-client';
+
+const name = 'User'
+
 export default function Chat() {
+  const socket = io();
+
+  const sendMessage = (message) => {
+    socket.emit('chat message', message);
+  }
+
+  socket.emit('joining msg', name);
+
+  socket.on('chat message', function(msg) {
+    console.log(msg);
+  });
+
   return (
     <div className="chat-container">
       <div className="chat-top">
         <ChatLog />
         <MembersList />
       </div>
-      <InputBox />
+      <InputBox onSend={sendMessage} />
     </div>
   )
 }
