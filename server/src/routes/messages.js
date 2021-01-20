fc1ff7929e56e279d5d44fbeec0a3e4847153df3
clarchiu <clarchiu@gmail.com>
2021-01-19 22:20:42 -0800

    created get most recent messages route

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -1,10 +1,43 @@
 const router = require("express").Router();
+const { saveMessage } = require('./helpers/messages');
 
 module.exports = (db) => {
+  
+  // Get num_msg most recent messages
+  router.get("/messages/:num_msg", (req, res) => {
+    const numMsg = req.params.num_msg;
+    db.query(`
+      SELECT 
+        T1.team_id,
+        sender_id,
+        name as sender,
+        message,
+        time_iso,
+        to_char(time_iso, 'Mon FMDD, YYYY at FMHH12:MI AM') as time_locale
+      FROM (
+        SELECT * 
+        FROM messages
+        ORDER BY time_iso DESC
+        LIMIT $1
+      ) T1
+      JOIN employees ON employees.id = sender_id
+      ORDER BY time_iso
+    `, [numMsg])
+    .then(data => {
+      res.json(data.rows);
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .send('Could not get messages from database', err);
+    })
+  });
+  
   // Get all messages
   router.get("/messages", (req, res) => {
     db.query(`
       SELECT 
+        messages.team_id,
         sender_id,
         name as sender,
         message,
@@ -24,22 +57,9 @@ module.exports = (db) => {
     })
   });
 
-  // Get most recent messages
-  
-
   // Post message
   router.post("/messages", (req, res) => {
-    const { senderId, teamId, sendTime, message } = req.body;
-    db.query(`
-      INSERT INTO messages (
-        sender_id,
-        team_id,
-        send_time,
-        message
-      )
-      VALUES ($1, $2, to_timestamp($3), $4)
-      RETURNING *
-    `, [senderId, teamId, sendTime / 1000, message])
+    saveMessage(db, req.body)
     .then(data => res.json(data.rows[0]))
     .catch(err => {
       res
