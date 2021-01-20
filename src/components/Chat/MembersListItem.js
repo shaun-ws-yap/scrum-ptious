348892460197f29441e15f20f3e9811b3ca586c4
clarchiu <clarchiu@gmail.com>
2021-01-20 01:23:17 -0800

    added css

--- src/components/Chat/MembersListItem.js
+++ src/components/Chat/MembersListItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import classNames from 'classnames';
+
 
 export default function MembersListItem(props) {
   const { name, role, avatar } = props;
+  const imgClass = classNames('chat-avatar', 'members-list-avatar', { online: false })
   return (
     <div className="members-list-item">
-      <img src={avatar} alt= "User Avatar" className="user-avatar"></img>
+      <img src={avatar} alt= "User Avatar" className={imgClass}></img>
       <div>
         <div>{name}</div>
         <div>{role}</div>
