a227fa023cc8c4b6d08c330665d9cdc96cf3cbe7
clarchiu <clarchiu@gmail.com>
2021-01-20 03:16:31 -0800

    reorder + green border on online users

--- src/components/Chat/MembersList.js
+++ src/components/Chat/MembersList.js
@@ -4,8 +4,13 @@ import MembersListItem from "./MembersListItem";
 
 const roles = ["Project Manager", "Developer"];
 
+const reorderArray = (arr, from, to) => {
+  arr.splice(to, 0, this.splice(from, 1)[0]);
+}
+
 export default function MembersList(props) {
-  const { teamUsers } = props;
+  const { teamUsers, onlineUsers } = props;
+
   const memberComponents = teamUsers.map(member => {
     const {id, name, role} = member;
     return (
@@ -14,12 +19,12 @@ export default function MembersList(props) {
         name={name} 
         role={roles[role-1]}
         avatar="https://randomuser.me/api/portraits/men/73.jpg"
+        online={onlineUsers.includes(id)}
       />
     );
   })
   return (
     <div className="members-list">
-      <h2>Members List</h2>
       {memberComponents}
     </div>
   )
--- src/components/Chat/MembersListItem.js
+++ src/components/Chat/MembersListItem.js
@@ -3,10 +3,11 @@ import classNames from 'classnames';
 
 
 export default function MembersListItem(props) {
-  const { name, role, avatar } = props;
-  const imgClass = classNames('chat-avatar', 'members-list-avatar', { online: false })
+  const { name, role, avatar, online } = props;
+  const imgClass = classNames('chat-avatar', 'members-list-avatar', { 'avatar-online': online })
+  const itemClass = classNames('members-list-item', { 'member-item-online': online })
   return (
-    <div className="members-list-item">
+    <div className={itemClass}>
       <img src={avatar} alt= "User Avatar" className={imgClass}></img>
       <div>
         <div>{name}</div>
