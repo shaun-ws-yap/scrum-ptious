a937bfb2b7deb135235f7693befdea4b247231c8
clarchiu <clarchiu@gmail.com>
2021-01-20 18:16:11 -0800

    added query to get messages before specified time

--- server/src/routes/queries/messages.js
+++ server/src/routes/queries/messages.js
@@ -10,7 +10,7 @@ const saveMessage = (db, messageData) => {
     VALUES ($1, $2, $3, $4)
     RETURNING *
   `, [sender_id, team_id, time_iso, message]);
-}
+};
 
 const getRecentMessages = (db, numMsg) => {
   return db.query(`
@@ -32,6 +32,27 @@ const getRecentMessages = (db, numMsg) => {
   `, [numMsg])
 };
 
+const getOlderMessages = (db, numMsg, time_iso) => {
+  return db.query(`
+    SELECT 
+      T1.team_id,
+      sender_id,
+      name as sender,
+      message,
+      time_iso,
+      to_char(time_iso, 'Mon FMDD, YYYY at FMHH12:MI AM') as time_locale
+    FROM (
+      SELECT * 
+      FROM messages
+      WHERE time_iso < $2
+      ORDER BY time_iso DESC
+      LIMIT $1
+    ) T1
+    JOIN employees ON employees.id = sender_id
+    ORDER BY time_iso
+  `, [numMsg, time_iso]);
+};
+
 const getAllMessages = db => {
   return db.query(`
     SELECT 
@@ -50,5 +71,6 @@ const getAllMessages = db => {
 module.exports = {
   saveMessage,
   getRecentMessages,
-  getAllMessages
+  getAllMessages,
+  getOlderMessages,
 }
