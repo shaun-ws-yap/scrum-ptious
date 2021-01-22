619d483fd74aec71678eb704f8dce880fdbe8dec
clarchiu <clarchiu@gmail.com>
2021-01-21 19:14:56 -0800

    restructured app + renamed some state fields

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -10,17 +10,48 @@ import TaskResource from './TaskResource';
 // import '../../styles/Dashboard.css';
 
 export default function Tasks(props) {
+  const {
+    setTaskItem,
+    role,
+    teamTasks,
+    tasks,
+    setTasks,
+    createTaskItem,
+    deleteTaskItem,
+    allTasks,
+    getUserTasks,
+    user,
+    teamUsers,
+    setTeamTasks
+  } = props
 
   return (
     <div className='dashboard'>
-
-
       <div className="dashboard-top">
-        <TaskProgress setTaskItem={props.setTaskItem} role={props.role} teamTasks={props.teamTasks} tasks={props.tasks} setTasks={props.setTasks} createTaskItem={props.createTaskItem} deleteTaskItem={props.deleteTaskItem} allTasks={props.allTasks} />
+        <TaskProgress 
+          setTaskItem={setTaskItem} 
+          role={role} 
+          teamTasks={teamTasks} 
+          tasks={tasks} 
+          setTasks={setTasks} 
+          createTaskItem={createTaskItem} 
+          deleteTaskItem={deleteTaskItem} 
+          allTasks={allTasks} 
+        />
       </div>
       <div className="dashboard-bottom">
-        { props.role === 1 && <MyTeam getUserTasks={props.getUserTasks} user={props.user} teamUsers={props.teamUsers} tasks={props.tasks} setTasks={props.setTasks} teamTasks={props.teamTasks} setTeamTasks={props.setTeamTasks} allTasks={props.allTasks} />}
-        { props.role === 2 && <TaskResource />}
+        { role === 1 && 
+          <MyTeam 
+            getUserTasks={getUserTasks} 
+            user={user} 
+            teamUsers={teamUsers} 
+            tasks={tasks} 
+            setTasks={setTasks} 
+            teamTasks={teamTasks} 
+            setTeamTasks={setTeamTasks} 
+            allTasks={allTasks} 
+          />}
+        { role === 2 && <TaskResource />}
       </div>
     </div>
   )
