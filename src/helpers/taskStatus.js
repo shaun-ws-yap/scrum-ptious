f427c3147f7cf27152e748eb90336981bff8fde8
kevinli296 <kevinli296@outlook.com>
2021-01-21 21:54:40 -0800

    Implement function to get all of teams employees task progress

--- src/helpers/taskStatus.js
+++ src/helpers/taskStatus.js
@@ -21,3 +21,32 @@ export function lateTaskStatus (data) {
 
   return counter;
 }
+
+export function teamTaskStatus (data, team, empID) {
+  let result = {
+    assigned: 0,
+    inProgress: 0,
+    inReview: 0,
+    late: 0,
+    complete: 0
+  };
+
+  for (const index of data) {
+    if (index.projecttask_id === team && index.employee_id === empID) {
+      if (index.status === 0) {
+        result.assigned += 1
+      } else if (index.status === 1) {
+        result.inProgress += 1;
+      } else if (index.status === 2) {
+        result.inReview += 1;
+      } else if (index.status === 3) {
+        result.complete += 1;
+      }
+      if (index.is_late === true) {
+        result.late += 1;
+      }
+    }
+  }
+
+  return result;
+}
