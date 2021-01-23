bcd64ce6cab7c854a4e26b48a5fcb3c9d6b47c16
Shaun Yap <me@shaunyap.ca>
2021-01-22 22:24:11 -0800

    Refactor new task to pop up a modal

--- src/components/Tasks/NewTaskItem.js
+++ src/components/Tasks/NewTaskItem.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import DatePicker from "react-datepicker";
+import { Modal, Button } from 'react-bootstrap';
 
 export default function NewTaskItem(props) {
   const [taskItem, setTaskItem] = useState({
@@ -10,11 +11,11 @@ export default function NewTaskItem(props) {
     due_date: new Date(),
   });
 
-  const teamMembersList = props.teamUsers.map(member => {
-    return (
-      <option value={member.id}>{member.name}</option>
-    )
-  })
+  const [show, setShow] = useState(false);
+
+  const teamMembersList = props.teamUsers.filter(user => user.role !== 1);
+
+  const handleClose = () => setShow(false);
 
   function reset() {
     setTaskItem({
@@ -23,6 +24,10 @@ export default function NewTaskItem(props) {
       employee_id: "",
       due_date: new Date(), 
     })
+  }
+
+  const getUserNameById = (id) => {
+    return props.teamUsers.filter(user => user.id === id)[0].name;
   }
 
   function validate() {
@@ -34,52 +39,72 @@ export default function NewTaskItem(props) {
 
     props.createTaskItem(taskItem);
     reset();
+    setShow(false);
   }
 
   return (
     <div>
-      <p>Create New Task</p>
+      <p onClick={() => setShow(true)}>Create New Task</p>
 
       <form 
         className="form-group"
         onSubmit={event => event.preventDefault()}
       >
-        <label for="title">Title:</label>
-        <input 
-          className="form-control"
-          name="title"
-          type="text"
-          value={taskItem.title}
-          onChange={event => setTaskItem(prevTaskItem => ({...prevTaskItem, title: event.target.value}))}
-        />
-        <label for="description">Description:</label>
-        <textarea
-          className="form-control"
-          name="description"
-          type="text"
-          value={taskItem.description}
-          onChange={event => setTaskItem(prevTaskItem => ({...prevTaskItem, description: event.target.value}))}
-        />
-        <label for ="employee">Assign to: </label>
-        <select class="form-control"
-          onChange={event => setTaskItem(prevTaskItem => ({...prevTaskItem, employee_id: Number(event.target.value)}))}
-        >
-          <option value={taskItem.employee_id} selected></option> 
-          {teamMembersList}
-        </select>
-        <label for ="due-date">Due on: </label>
-        <DatePicker 
-          className="form-control" 
-          selected={new Date(taskItem.due_date)} 
-          showTimeSelect
-          onChange={date => setTaskItem(prevTaskItem => ({...prevTaskItem, due_date: date}))}
-        />
-        <button 
-          className="btn btn-primary"
-          onClick={event => (validate())}
-        >
-          Submit
-        </button>
+        <Modal show={show}  onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <h3>New Task</h3>
+            </Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <label for="title">Title: </label>
+            <input
+              name="title"
+              className="form-control"
+              value={taskItem.title}
+              onChange={(event) => setTaskItem(prev => ({...prev, title: event.target.value}))}
+            />
+
+            <label for="description">Description: </label>
+            <textarea
+              name="description"
+              className="form-control"
+              value={taskItem.description}
+              onChange={(event) => setTaskItem(prev => ({...prev, description: event.target.value}))}
+            />
+
+            <label for="assignTo">Assign to: </label>
+            <select class="form-control"
+              onChange={event => setTaskItem(prevTaskItem => ({...prevTaskItem, employee_id: Number(event.target.value)}))}
+            >
+              <option selected value={""}></option>
+              { teamMembersList.map(member => {
+                return (
+                  <option value={member.id}>{member.name}</option>
+                )
+              })  }
+            </select>
+
+            <label for ="due-date">Due on: </label>
+            <DatePicker 
+              className="form-control" 
+              selected={new Date(taskItem.due_date)} 
+              showTimeSelect
+              onChange={date => setTaskItem(prevTaskItem => ({...prevTaskItem, due_date: date}))}
+            />
+
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button 
+              variant="primary"
+              onClick={event => (validate())}
+            >
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </form>
     </div>
 
