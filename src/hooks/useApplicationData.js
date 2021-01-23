e5071a59ab083f04de2e9d20249f4940296ecbeb
Shaun Yap <me@shaunyap.ca>
2021-01-22 18:42:59 -0800

    Add edit task item feature

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -80,11 +80,20 @@ export default function useApplicationData(socket, loginToken) {
   function editTaskItem(taskItem) {
     return axios.put(`http://localhost:8080/api/tasks/${taskItem.id}`, taskItem)
     .then(res => {
-      const id = taskItem.id;
-      const newTasks = [...state.allTasks];
-      newTasks[id] = taskItem;
-      setState(prev => ({...prev, allTasks: newTasks, teamTasks: newTasks}));
-      console.log(state.allTasks)
+      const taskItemIndex = state.allTasks.findIndex(x => x.id === taskItem.id);
+      setState(prev => ({
+        ...prev,
+        teamTasks: [
+          ...state.teamTasks.slice(0, taskItemIndex),
+          Object.assign({}, state.teamTasks[taskItemIndex], taskItem),
+          ...state.teamTasks.slice(taskItemIndex + 1)
+        ],
+        allTasks: [
+          ...state.allTasks.slice(0, taskItemIndex),
+          Object.assign({}, state.allTasks[taskItemIndex], taskItem),
+          ...state.allTasks.slice(taskItemIndex + 1)
+        ]
+      }))
     })
     .catch(e => console.log(e));
   }
