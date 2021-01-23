6f52a364746bee5cfb8425bef7991b1a0a00d9ee
clarchiu <clarchiu@gmail.com>
2021-01-22 22:51:37 -0800

    pulled out filtering tasks

--- src/helpers/filterTasksByUser.js
+++ src/helpers/filterTasksByUser.js
@@ -1,6 +1,4 @@
 export default function filterTasksByUser(uid, tasks) {
   console.log(typeof uid);
-  const toRet = tasks.filter(task => task.employee_id === Number(uid));
-  console.log(toRet);
-  return toRet;
+  return tasks.filter(task => task.employee_id === Number(uid));
 }
