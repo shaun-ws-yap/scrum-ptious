7b7dc70beab23621d820385a58e0c79d55c9ea08
Shaun Yap <me@shaunyap.ca>
2021-01-23 02:39:17 -0800

    Tidy up performance review component

--- src/components/Performance-Review/Feedback.js
+++ src/components/Performance-Review/Feedback.js
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getEmployeeName } from '../../helpers/getEmployeeName';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
+import Moment from 'react-moment';
 
 export default function Feedback(props) {
 
   const { teamUsers, taskItem } = props;
+  const [feedback, setFeedback] = useState({
+    message: "",
+  });
+
+  const handleClose = () => {
+    props.setShow(false);
+  }
+
+  const handleSubmit = () => {
+    console.log(taskItem, feedback.message)
+    // TODO: send to server
+    setFeedback(prev => ({...prev, message: ""}));
+    props.setShow(false);
+  }
 
   return (
-    <div className="task-progress">
-      <ul>
-      <h4>{taskItem.title}</h4>
-      <h4>{taskItem.description}</h4>
-      <h4>{getEmployeeName(teamUsers, taskItem.employee_id)}</h4>
-      <h4>{taskItem.is_late && 'LATE'}</h4>
-      </ul>
-      <div className="feedback-textbox">
-        <form>
-        <textarea />
-      <Button
-      confirm variant="primary"
-      onClick={() => console.log('Pressed')}>
-        Submit
-        </Button>
+
+    <div className="feedback-container">
+      <Modal show={props.show} onHide={handleClose} >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {taskItem.title}
+          </Modal.Title>
+        </Modal.Header>
+
+        <form class="form-group" onSubmit={event => event.preventDefault()}>
+          <Modal.Body>
+            <p>{taskItem.description}</p>
+            <p>Assigned to: {getEmployeeName(teamUsers, taskItem.employee_id)}</p>
+            <p>On: 
+              <Moment format="Do MMM YYYY h:mm A" >{taskItem.creation_date}</Moment>
+            </p>
+            <p>Due on: 
+              <Moment format="Do MMM YYYY h:mm A" >{taskItem.due_date}</Moment>
+              { taskItem.is_late && (<span className="badge badge-danger">LATE</span>) }
+            </p>
+            
+            <div>
+              <label for="feedback">Feedback:</label>
+              <textarea
+                name="feedback"
+                className="form-control"
+                value={feedback.message}
+                onChange={event => setFeedback(prev => ({...prev, message: event.target.value}))}
+              />
+            </div>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button 
+              variant="primary"
+              onClick={(event) => handleSubmit()}
+            >
+              Submit
+            </Button>
+          </Modal.Footer>
         </form>
-      </div>
+      </Modal>
     </div>
   )
 }
--- src/components/Performance-Review/TaskListItem.js
+++ src/components/Performance-Review/TaskListItem.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 export default function TaskListItem(props) {
 
-  const { title, name, description, is_late, is_viewed, taskData, setTaskItem } = props;
+  const { title, name, description, is_late, is_viewed, taskData, setTaskItem, setShow } = props;
+
+  const handleOpen = () => {
+    setTaskItem(taskData);
+    setShow(true);
+  }
 
   return (
-    <li onClick={() => setTaskItem(taskData)}>
+    <li onClick={() => handleOpen()}>
       <h4>{title}</h4>
       <h4>{name}</h4>
       <h4>{description}</h4>
