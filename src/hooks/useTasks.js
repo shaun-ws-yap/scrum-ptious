cc06062e2f75fdaf8cbc845a10fe136f586d9b3a
clarchiu <clarchiu@gmail.com>
2021-01-24 03:08:09 -0800

    on feedback event set state

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
+import findSubmissionByTask from '../helpers/findSubmissionByTask';
 
-export default function useTasks(loginToken, socket, taskSetters, setNotification ) {
+export default function useTasks(loginToken, socket, submissions, taskSetters, setNotification ) {
   const TASK_STATUS = {
     ASSIGNED: 0,
     IN_PROGRESS: 1,
@@ -27,12 +28,14 @@ export default function useTasks(loginToken, socket, taskSetters, setNotificatio
     socket.on('employee submit', result => {
       console.log(result);
       setTasks(result.teamTasks);
-      setSubmissions(result.submission);
+      setSubmissions(result.submissions);
       //setSubmissions(result.submission);
     });
 
     socket.on('feedback', result => {
       console.log(result);
+      setTasks(result.teamTasks);
+      setSubmissions(result.submissions);
     })
       
     return () => {
@@ -72,9 +75,22 @@ export default function useTasks(loginToken, socket, taskSetters, setNotificatio
     socket.emit('employee submit', submitTaskData);
   };
 
-  const giveFeedback = (submissionId, message, taskId, accepted) => {
-    const status = accepted ? 3 : 1;
-    socket.emit('feedback', submissionId, message, taskId, status);
+  const giveFeedback = (message, task, accepted) => {
+    const ACCEPTED = 'accepted';
+    const REJECTED = 'rejected';
+
+    const submission = findSubmissionByTask(submissions, task.id);
+    const feedback = {
+      ...submission,
+      feedback_string: message,
+      status: accepted ? ACCEPTED : REJECTED
+    }
+    const taskItem = {
+      ...task,
+      status: accepted ? 3 : 1
+    }
+
+    socket.emit('feedback', {feedback, taskItem});
   }
 
   return {
