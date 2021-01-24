cafe5478bb13501018da769fb9ab95d23fcac46c
clarchiu <clarchiu@gmail.com>
2021-01-23 22:33:28 -0800

    add func that saves submssion and updates task tgt

--- server/src/routes/queries/submissions.js
+++ server/src/routes/queries/submissions.js
@@ -1,3 +1,15 @@
+const { updateTaskStatus, getTasksByTeam } = require('./tasks');
+
+const getAllSubmissions = db => {
+  const queryString = `
+  SELECT
+  *
+  FROM submissions
+  ORDER BY submission_date
+  `;
+  return db.query(queryString);
+}
+
 // Create submission
 const saveSubmission = (db, submission) => {
   const queryString = `
@@ -9,6 +21,25 @@ const saveSubmission = (db, submission) => {
   return db.query(queryString, [feedback_string, submission_date, task_id]);
 };
 
+// use this for submitting task
+const submitTask = (db, submitTaskData) => {
+  const { submission, taskItem } = submitTaskData;
+  const getUpdatedTasks = updateTaskStatus(db, taskItem.id, taskItem.status)
+    .then(data => getTasksByTeam(db, data.rows[0].projecttask_id));
+
+  return Promise.all([
+    getUpdatedTasks,
+    saveSubmission(db, submission)
+  ])
+    .then(([teamTasksData, submissionData]) => {
+      return {
+        teamTasks: teamTasksData.rows,
+        submission: submissionData.rows[0]
+      };
+    });
+}
+
 module.exports = {
-  saveSubmission
+  getAllSubmissions,
+  submitTask,
 }
