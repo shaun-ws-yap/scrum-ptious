2e49d5a4615ce7902bc9b6c529df0b3321030626
clarchiu <clarchiu@gmail.com>
2021-01-24 04:12:42 -0800

    removed redundant all tasks

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -7,15 +7,15 @@ import EmployeeProgress from './EmployeeProgress';
 import '../../styles/Dashboard.css';
 
 export default function Dashboard(props) {
-  const { tasks, teamTasks, teamUsers, allTasks, role } = props;
+  const { tasks, teamTasks, teamUsers, role } = props;
 
   return (
     <div className='dashboard'>
       <div className="dashboard-top">
-        <ProjectProgress teamTasks={teamTasks} allTasks={allTasks} />
+        <ProjectProgress teamTasks={teamTasks} />
       </div>
       <div className="dashboard-bottom">
-      {role === 1 && <EmployeeProgress independentTasks={tasks} teamTasks={teamTasks} allTasks={allTasks} teamUsers={teamUsers} />}
+      {role === 1 && <EmployeeProgress independentTasks={tasks} teamTasks={teamTasks} teamUsers={teamUsers} />}
       {role === 2 && <IndependentProgress independentTasks={tasks} />}
       </div>
     </div>
