37b382d2a6622e58b37f860e5f326a7bb0b0c8b7
Shaun Yap <me@shaunyap.ca>
2021-01-25 16:32:47 -0800

    setup for heroku deployment

--- server/src/index.js
+++ server/src/index.js
@@ -34,7 +34,11 @@ db.connect(err => {
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use("/api", messageRoutes(db));
 app.use("/api", employeeRoutes(db));
