3290b462d20ceeb83324fc2322dee0324607e7eb
Shaun Yap <me@shaunyap.ca>
2021-01-27 04:11:43 -0800

    fix user panel scaling

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import Submissions from './Submissions';
 import Sidebar from './Sidebar';
 import UserPanel from './UserPanel';
 import Login from './Login';
+import SlidingPanel from 'react-sliding-side-panel';
 
 import useApplicationData from '../hooks/useApplicationData';
 import useNotifications from '../hooks/useNotifications';
@@ -164,7 +165,7 @@ function App() {
             userInfo={userInfo} 
             tasks={teamTasks} 
             teamUsers={teamUsers} 
-          /> 
+          />
         </section>
       {/* </div> */}
     </div>
