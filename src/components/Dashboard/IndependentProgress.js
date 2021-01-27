ca98f6b32f15167ce1819331a714643253f7c0a8
Shaun Yap <me@shaunyap.ca>
2021-01-26 22:05:42 -0800

    independent progress css

--- src/components/Dashboard/IndependentProgress.js
+++ src/components/Dashboard/IndependentProgress.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Doughnut, Bar } from 'react-chartjs-2';
+import { Doughnut, Bar, Line } from 'react-chartjs-2';
 import { taskStatus, lateTaskStatus } from '../../helpers/taskStatus';
 
 export default function IndependentProgress(props) {
@@ -12,47 +12,95 @@ export default function IndependentProgress(props) {
 
 
   return (
-    <div className="independent-progress">
-      <Bar
-        data={{
-          labels: ['Assigned', 'In-progress', 'In-review', 'Late', 'Complete'],
-          datasets: [
-            {
-              label: '# of tasks',
-              data: [assignedTasks, inProgressTasks, inReviewTasks, lateTasks, completeTasks],
-              backgroundColor: [
-                'rgba(28, 20, 255, 0.8)',
-                'rgba(255, 247, 20, 0.8)',
-                'rgba(232, 86, 2, 0.8)',
-                'rgba(255, 20, 20, 0.8)',
-                'rgba(33, 232, 2, 0.8)',
-              ]
-            }
-          ],
-        }}
-        height={400}
-        width={600}
-        options={{
-          maintainAspectRatio: false,
-          title: {
-            display: true,
-            text: 'Your Progress',
-            fontSize: 25,
-            fontFamily: 'Poppins'
-          },
-          legend: {
-            display: true,
-            position: 'right'
-          },
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
+    <div className="employee-chart">
+
+      <div className="independent-progress">
+        <div className="bar-chart">
+          <Bar
+            data={{
+              labels: ['Assigned', 'In-progress', 'In-review', 'Late', 'Complete'],
+              datasets: [
+                {
+                  label: '# of tasks',
+                  data: [assignedTasks, inProgressTasks, inReviewTasks, lateTasks, completeTasks],
+                  backgroundColor: [
+                    'rgba(28, 20, 255, 0.8)',
+                    'rgba(255, 247, 20, 0.8)',
+                    'rgba(232, 86, 2, 0.8)',
+                    'rgba(255, 20, 20, 0.8)',
+                    'rgba(33, 232, 2, 0.8)',
+                  ]
+                }
+              ],
+            }}
+            height={400}
+            width={600}
+            options={{
+              maintainAspectRatio: false,
+              title: {
+                display: true,
+                text: 'Your Progress',
+                fontSize: 25,
+                fontFamily: 'Poppins'
+              },
+              legend: {
+                display: true,
+                position: 'right'
+              },
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
               }
-            }]
-          }
-        }}
-      />
+            }}
+          />
+        </div>
+
+        <div className="line-chart">
+          <Line 
+            data={{
+              labels: ['Assigned', 'In-progress', 'In-review', 'Late', 'Complete'],
+              datasets: [
+                {
+                  label: '# of tasks',
+                  data: [assignedTasks, inProgressTasks, inReviewTasks, lateTasks, completeTasks],
+                  backgroundColor: [
+                    'rgba(28, 20, 255, 0.8)',
+                    'rgba(255, 247, 20, 0.8)',
+                    'rgba(232, 86, 2, 0.8)',
+                    'rgba(255, 20, 20, 0.8)',
+                    'rgba(33, 232, 2, 0.8)',
+                  ]
+                }
+              ],
+            }}
+            height={400}
+            width={600}
+            options={{
+              maintainAspectRatio: false,
+              title: {
+                display: true,
+                text: 'Your Progress',
+                fontSize: 25,
+                fontFamily: 'Poppins'
+              },
+              legend: {
+                display: true,
+                position: 'right'
+              },
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
+              }
+            }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
